83e9199e0b5e00d3e559629f7950063cffd22948
CalinaThalya <calina.thalya@icloud.com>
2024-11-12 00:28:31 -0300

    first commit

---
+++ src/main/java/br/com/fiap/hydriomind/repository/EmpresaRepository.java
@@ -0,0 +1,10 @@
+package br.com.fiap.hydriomind.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.fiap.hydriomind.entity.Empresa;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+}
